afb001a22fba16852a877e000e864a407b024874
Ezell <lynellf@gmail.com>
2023-01-10 11:54:13 -0600

    move main tag

--- pages/index.js
+++ pages/index.js
@@ -12,9 +12,7 @@ export default function Home() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <Landing />
-      </main>
+      <Landing />
     </div>
   );
 }
