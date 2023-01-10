77b9195e79938051ff69e39040b7cf2ca0c40f36
Ezell <lynellf@gmail.com>
2023-01-10 11:53:48 -0600

    implement sidebar

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,40 @@
-import { MantineProvider, AppShell, Header } from "@mantine/core";
+import styled from "styled-components";
+import { MantineProvider } from "@mantine/core";
 import Appbar from "components/Appbar";
+import Navbar from "components/Navbar";
+import pixelsToRem from "utils/pixelsToRem";
+
+const AppShell = styled.div`
+  display: grid;
+  grid-template-columns: 100%;
+  grid-template-rows: ${pixelsToRem(120)} 1fr;
+
+  @media (min-width: 768px) {
+    grid-template-columns: ${pixelsToRem(200)} 1fr;
+    grid-template-rows: 100%;
+  }
+`;
+
+const Container = styled.div`
+  padding: 0 1rem;
+`;
 
 function MyApp({ Component, pageProps }) {
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
-      <AppShell header={<Appbar />}>
-        <Component {...pageProps} />
-      </AppShell>
+      <header>
+        <Appbar />
+      </header>
+      <main>
+        <AppShell>
+          <aside>
+            <Navbar />
+          </aside>
+          <Container>
+            <Component {...pageProps} />
+          </Container>
+        </AppShell>
+      </main>
     </MantineProvider>
   );
 }
